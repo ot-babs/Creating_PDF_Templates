81141affbef445eb0cfb2a679e1563f0781b5266
ot-babs <toyosi.babs@gmail.com>
2023-08-17 22:46:52 +0100

    Add lines to all pages of the pdf

--- main.py
+++ main.py
@@ -9,28 +9,30 @@ df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=12)
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     for i in range(20,298,10):
         pdf.line(10, i, 200, i)
 
     # Set the footer
     pdf.ln(265)
-    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
 
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
-    # Set the footer
-    pdf.ln(277)
-    pdf.set_font(family="Times", style="B", size=12)
-    pdf.set_text_color(180, 180, 180)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-    for y in range(20,298,10):
-        pdf.line(10, y, 200, y)
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 
 
